b6b2a873aa84d40b1e9dce647c0ce151b9b9f83a
llthompson <lisalacythompson+github@gmail.com>
2023-06-01 21:05:32 -0500

    filtering logic

--- scripts.js
+++ scripts.js
@@ -1,26 +1,7 @@
-// We'll pre-populate this array with a couple objects just so it's not undefined if your internet connection isn't working properly.
-
-let arrayOfTodos = [
-    {
-        "userId": 14,
-        "id": 1,
-        "title": "delectus aut autem",
-        "completed": false
-    },
-    {
-        "userId": 20,
-        "id": 2,
-        "title": "delectus aut autem",
-        "completed": false
-    }]
-
-// const fetchTodos = () => {
-//     fetch('https://jsonplaceholder.typicode.com/todos')
-//     .then( (response) => response.json())
-//     .then( (json) => arrayOfTodos = json)
-// }
+let arrayOfTodos = [];
 
 const fetchTodos = () => {
+    console.log('fetchTodos')
     return fetch('https://jsonplaceholder.typicode.com/todos');
 }
 
@@ -33,17 +14,71 @@ const logTodos = () => {
     console.log(arrayOfTodos)
 }
 
+
+
+
 const populateTodos = () => {
     fetchTodos()
         .then(res => res.json())
         .then(todos => {
+            console.log('assigning to array')
+            arrayOfTodos = todos; // Store todos in the arrayOfTodos variable
+            console.log(JSON.stringify(arrayOfTodos))
             const todoList = document.getElementById('todo-list');
             todoList.innerHTML = '';
-
-            todos.forEach(todo => {
+            arrayOfTodos.forEach(todo => {
                 const li = document.createElement('li');
                 li.textContent = todo.title;
                 todoList.appendChild(li);
             });
-        })
+
+
+        });
+}
+
+const getUserID = () => {
+    const userIdInput = document.getElementById('user-id-input');
+    return parseInt(userIdInput.value);
+}
+
+let filteredTodos = [];
+// filteredTodos = todos.filter(todo => todo.userId === getUserID());
+
+const filterTodos = () => {
+    const userID = getUserID()
+    const todoList = document.getElementById('todo-list');
+    todoList.innerHTML = '';
+    filteredTodos = arrayOfTodos.filter(todo => todo.userId === userID);
+    filteredTodos.forEach(todo => {
+        const li = document.createElement('li');
+        li.textContent = todo.title;
+        todoList.appendChild(li);
+    });
+    const completedBtn = document.getElementById('completed-todos-btn');
+    const notCompletedBtn = document.getElementById('not-completed-todos-btn');
+    completedBtn.style.display = filteredTodos.length > 0 ? 'block' : 'none';
+    notCompletedBtn.style.display = filteredTodos.length > 0 ? 'block' : 'none';
+}
+
+
+const showCompletedTodos = () => {
+    const todoList = document.getElementById('todo-list');
+    todoList.innerHTML = '';
+    const completedTodos = filteredTodos.filter(todo => todo.completed);
+    completedTodos.forEach(todo => {
+        const li = document.createElement('li');
+        li.textContent = todo.title;
+        todoList.appendChild(li);
+    });
+}
+
+const showNotCompletedTodos = () => {
+    const todoList = document.getElementById('todo-list');
+    todoList.innerHTML = '';
+    const notCompletedTodos = filteredTodos.filter(todo => !todo.completed);
+    notCompletedTodos.forEach(todo => {
+        const li = document.createElement('li');
+        li.textContent = todo.title;
+        todoList.appendChild(li);
+    });
 }
